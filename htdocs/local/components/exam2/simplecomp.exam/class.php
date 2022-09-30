50fdead89f6a5f7c75077f9ff409f35d1a4d9c16
Utya92 <truejuve@gmail.com>
2022-09-30 20:52:19 +0300

    ex2-70

---
+++ htdocs/local/components/exam2/simplecomp.exam/class.php
@@ -0,0 +1,152 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Loader,
+    Bitrix\Iblock;
+
+class MySimpleCatalog extends CBitrixComponent {
+
+    public function onPrepareComponentParams($arParams) {
+        if (isset($arParams["CACHE_TIME"])) {
+            $arParams["CACHE_TIME"] = 36000000;
+        }
+        if (isset($arParams["PRODUCTS_IBLOCK_ID"])) {
+            $arParams["CACHE_TIME"] = 0;
+        }
+        if (isset($arParams["NEWS_IBLOCK_ID"])) {
+            $arParams["CACHE_TIME"] = 0;
+        }
+        return $arParams;
+    }
+
+    public function checkErrorInModule() {
+        if (!Loader::includeModule("iblock")) {
+            ShowError(GetMessage("SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"));
+        }
+    }
+    public function executeComponent() {
+        global $APPLICATION;
+        $this->checkErrorInModule();
+        $this->render();
+        $APPLICATION->SetTitle(GetMessage("COUNT") . $this->arResult["PRODUCT_CNT"]);
+    }
+
+
+    function getNews(): array {
+        $arNews = array();
+        $arNewsId = array();
+
+        //список активных новостей инфоблока новости
+        $obNews = CIBlockElement::GetList(
+            array(),
+            array(
+                "IBLOCK_ID" => $this->arParams["NEWS_IBLOCK_ID"],
+                "ACTIVE" => "Y"
+            ),
+            false,
+            false,
+            //ограничение вывода полей элементов
+            array(
+                "NAME",
+                "ACTIVE_FROM",
+                "ID"
+            )
+        );
+        while ($element = $obNews->Fetch()) {
+            $arNewsId[] = $element["ID"];
+            $arNews[$element["ID"]] = $element;
+        }
+        $output["AR_NEWS"] = $arNews;
+        $output["AR_NEWS_ID"] = $arNewsId;
+        return $output;
+    }
+
+
+    function getSections($newsId): array {
+        $arSections = array();
+        $arSectionsID = array();
+        //список активных разделов с привязкой к активным новостям
+        $obSection = CIBlockSection::GetList(
+            array(),
+            array(
+                "IBLOCK_ID" => $this->arParams["PRODUCTS_IBLOCK_ID"],
+                "ACTIVE" => "Y",
+                $this->arParams["PRODUCTS_IBLOCK_ID_PROPERTY"] => $newsId
+            ),
+            false,
+            array(
+                "NAME",
+                "IBLOCK_ID",
+                "ID",
+                $this->arParams["PRODUCTS_IBLOCK_ID_PROPERTY"]
+            ),
+            false
+        );
+
+        while ($arSectionCatalog = $obSection->Fetch()) {
+            $arSectionsID[] = $arSectionCatalog["ID"];
+            $arSections[$arSectionCatalog["ID"]] = $arSectionCatalog;
+        }
+        $output["AR_SECTION"] = $arSections;
+        $output["AR_SECTION_ID"] = $arSectionsID;
+
+        return $output;
+    }
+
+    function getProducts($arSectionsID, $arSections, &$arNews) {
+        $obProduct = CIBlockElement::GetList(
+            array(),
+            array(
+                "IBLOCK_ID" => $this->arParams["PRODUCTS_IBLOCK_ID"],
+                "ACTIVE" => "Y",
+                "SECTION_ID" => $arSectionsID
+            ),
+            false,
+            false,
+            //ограничение вывода полей элементов
+            array(
+                "NAME",
+                "IBLOCK_SECTION_ID",
+                "ID",
+                "IBLOCK_ID",
+                "PROPERTY_ARTNUMBER",
+                "PROPERTY_MATERIAL",
+                "PROPERTY_PRICE"
+            )
+        );
+
+        $this->arResult["PRODUCT_CNT"] = 0;
+
+        while ($arProduct = $obProduct->Fetch()) {
+            $this->arResult["PRODUCT_CNT"]++;
+            foreach ($arSections[$arProduct["IBLOCK_SECTION_ID"]][$this->arParams["PRODUCTS_IBLOCK_ID_PROPERTY"]] as $newsId) {
+                $arNews[$newsId]["PRODUCTS"][] = $arProduct;
+            }
+        }
+        $this->setResultCacheKeys(array("PRODUCT_CNT"));
+    }
+
+    function render() {
+        if ($this->startResultCache()) {
+            $arNews = $this->getNews()["AR_NEWS"];
+            $arNewsId = $this->getNews()["AR_NEWS_ID"];
+
+            $arSection = $this->getSections($arNewsId)["AR_SECTION"];
+            $arSectionId = $this->getSections($arNewsId)["AR_SECTION_ID"];
+
+            $this->getProducts($arSectionId, $arSection, $arNews);
+
+            foreach ($arSection  as $i) {
+                foreach ($i[$this->arParams["PRODUCTS_IBLOCK_ID_PROPERTY"]] as $val) {
+                    $arNews[$val]["SECTIONS"] [] = $i["NAME"];
+                }
+            }
+
+            $this->arResult["NEWS"] = $arNews;
+//            var_dump($arNews) ;
+        } else {
+            $this->abortResultCache();
+        }
+        $this->includeComponentTemplate();
+    }
+}
---
+++ htdocs/local/components/exam2/simplecomp.exam/templates/.default/template.php
@@ -0,0 +1,31 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
+<?php
+if (count($arResult["NEWS"]) > 0) { ?>
+    <ul>
+        <?php foreach ($arResult["NEWS"] as $new) { ?>
+            <li>
+                <b>
+                    <?= $new["NAME"]; ?>
+                </b>
+                <?= $new["ACTIVE_FROM"]; ?>
+                (<?= implode(", ", $new["SECTIONS"]); ?>)
+            </li>
+            <?php if (count($new["PRODUCTS"]) > 0) {
+                ?>
+                <ul>
+                    <?php foreach ($new["PRODUCTS"] as $arProduct) {
+                        ?>
+                        <li>
+                            <?= $arProduct["NAME"]; ?>
+                            <?= $arProduct["PROPERTY_PRICE_VALUE"]; ?>
+                            <?= $arProduct["PROPERTY_MATERIAL_VALUE"]; ?>
+                            <?= $arProduct["PROPERTY_ARTNUMBER_VALUE"]; ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            <?php } ?>
+        <?php } ?>
+    </ul>
+<?php } ?>
+
