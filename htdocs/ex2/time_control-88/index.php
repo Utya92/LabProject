cd647208ae3c3a69140e764cac5e423bbb3be6de
Utya92 <truejuve@gmail.com>
2022-10-07 17:13:53 +0300

    ex2-88

---
+++ htdocs/ex2/time_control-88/index.php
@@ -0,0 +1,13 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности 88");
+?>
+    Самая нагруженная страница: /products/index.php. Процент нагрузки 18.97
+    <br>
+    Работа компонента по умолчанию: 20 КБ
+    <br>
+    Работа компонента c параметром  $resultCacheKeys = array("ID"): 12 КБ
+    <br>
+    Разница 8кб
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+
