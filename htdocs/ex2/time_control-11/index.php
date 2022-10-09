a218195923656fd8fd81637efd209ba0ceef2e80
Utya92 <truejuve@gmail.com>
2022-10-09 20:22:38 +0300

    ex2-11

---
+++ htdocs/ex2/time_control-11/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности 11");
+?>
+    Самая нагруженная страница: /products/index.php. среднее время исполнения 1805 сек.
+    <br>
+    Проблемный компонент: bitrix:catalog.section. Запросов 34
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
